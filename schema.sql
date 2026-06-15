f19072c8a500a9c50854a96ed7b6bd8ed314d370
Jannatul Ferdoush <jannatferdoush1212@gmail.com>
2026-06-15 16:15:02 +0600

    Table 2 is created

--- schema.sql
+++ schema.sql
@@ -2,24 +2,47 @@
 DROP TABLE IF EXISTS Bookings;
 DROP TABLE IF EXISTS Matches;
 DROP TABLE IF EXISTS Users;
-
 -- ================================================================================
 -- tables creation
 -- ================================================================================
 -- 1. Create Users table start
 -- ================================================================================
 CREATE TABLE Users (
-  user_id SERIAL,
-  full_name varchar(100) NOT NULL,
-  email varchar(200) NOT NULL,
-  role varchar(20) NOT NULL,
-  phone_number varchar(20),
-  CONSTRAINT pk_users PRIMARY KEY (user_id),
-  CONSTRAINT uq_users_email UNIQUE (email),
-  CONSTRAINT chk_users_role CHECK (role IN ('Ticket Manager', 'Football Fan'))
+    user_id SERIAL,
+    full_name varchar(100) NOT NULL,
+    email varchar(200) NOT NULL,
+    role varchar(20) NOT NULL,
+    phone_number varchar(20),
+    CONSTRAINT pk_users PRIMARY KEY (user_id),
+    CONSTRAINT uq_users_email UNIQUE (email),
+    CONSTRAINT chk_users_role CHECK (role IN ('Ticket Manager', 'Football Fan'))
+);
+-- ================================================================================
+-- 1. Create Users table end
+-- ================================================================================
+
+-- ================================================================================
+-- 2. Create Matches table start
+-- ================================================================================
+CREATE TABLE Matches (
+  match_id SERIAL,
+  fixture varchar(150) NOT NULL,
+  tournament_category varchar(100) NOT NULL,
+  base_ticket_price numeric(10, 2) NOT NULL,
+  match_status varchar(20) NOT NULL,
+  CONSTRAINT pk_matches PRIMARY KEY (match_id),
+  CONSTRAINT chk_base_ticket_price_is_positive CHECK (base_ticket_price >= 0),
+  CONSTRAINT chk_match_status CHECK (
+    match_status IN (
+      'Available',
+      'Selling Fast',
+      'Sold Out',
+      'Postponed'
+    )
+  )
 );
 
 
 -- ================================================================================
--- 1. Create Users table end
+-- 2. Create Matches table end
 -- ================================================================================
